2d547ba9810da509738c3b692019215e6df4545f
MarcPuigdemont <marcpuigdemontriera@gmail.com>
2019-03-26 20:15:33 +0100

    Improved coverage reports

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest',
   },
   transformIgnorePatterns: ['/node_modules/'],
@@ -10,8 +11,14 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   snapshotSerializers: ['jest-serializer-vue'],
-  testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
+  ],
   testURL: 'http://localhost/',
   collectCoverage: true,
+  collectCoverageFrom: [
+    'src/components/**/*.{js,vue}',
+    'src/views/**/*.{js,vue}',
+  ],
   coverageReporters: ['json', 'html'],
 };
