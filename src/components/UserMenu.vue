77f59fa90892f5465bc0046a6734d88f77ea566b
MarcPuigdemont <marcpuigdemontriera@gmail.com>
2019-04-03 22:40:45 +0200

    Moved form fields to an object and using mapGetters. Removed a piece of css in favor of vuetify classes

--- src/components/UserMenu.vue
+++ src/components/UserMenu.vue
@@ -5,40 +5,44 @@
       <v-btn flat @click="$router.push({ name: 'register' })">Register</v-btn>
     </div>
     <v-layout v-if="!loggedIn && loginFormVisible" row class="flex-none">
-      <v-text-field v-model="username" label="username" class="login-input"/>
-      <v-text-field v-model="password" label="password" type="password" class="login-input"/>
+      <v-text-field v-model="loginForm.username" label="username" class="login-input ml-2"/>
+      <v-text-field
+        v-model="loginForm.password"
+        label="password"
+        type="password"
+        class="login-input ml-2"
+      />
       <v-btn flat @click="login">Login</v-btn>
       <v-btn flat @click="loginFormVisible = false;">Cancel</v-btn>
     </v-layout>
     <v-layout v-if="loggedIn" row align-center class="flex-none">
-      <span>Welcome {{ user }},&nbsp;</span>
+      <span>Welcome {{ username }},&nbsp;</span>
       <a href="#" @click.prevent="logout">Logout</a>
     </v-layout>
   </v-layout>
 </template>
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   data() {
     return {
       loginFormVisible: false,
-      username: undefined,
-      password: undefined,
+      loginForm: {
+        username: undefined,
+        password: undefined,
+      },
     };
   },
   computed: {
-    loggedIn() {
-      return this.$store.getters.loggedIn;
-    },
-    user() {
-      return this.$store.getters.username;
-    },
+    ...mapGetters(['loggedIn', 'username']),
   },
   methods: {
     login() {
       this.$store
         .dispatch('login', {
-          username: this.username,
-          password: this.password,
+          username: this.loginForm.username,
+          password: this.loginForm.password,
         })
         .then(() => {
           this.loginFormVisible = false;
@@ -60,6 +64,5 @@ export default {
 .login-input {
   max-height: 50px;
   max-width: 120px;
-  margin-left: 8px !important;
 }
 </style>
